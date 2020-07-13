c4349976fbe1516783d7978f38a521a56085f5ac
George Kochera <george.kochera@gmail.com>
2020-07-13 00:05:16 -0400

    added error handling to app.js so website doesn't crash when db cannot connect

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ Database Setup and Configuration
 */
 
 const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
+handleDisconnect(connection)
 
 /*
 Routing
@@ -53,10 +54,15 @@ app.get('/', function(req, res) {
     // Simple query to make sure the database is connected.
     var data = 'ClearDB Connected. Users are: '
     connection.query('SELECT * FROM `users`', function(error, results, fields){
+        if (error) {
+            data = 'ClearDB is down!'
+            res.render('home', {data: data})
+        } else {
         results.forEach(element => {
             data += element.first_name + ' '
         });
         res.render('home', {data: data})    
+    }
     })
     
 })
@@ -66,3 +72,23 @@ Listener
 */
 
 app.listen(app.get('port'))
+
+/*
+Error Handling
+*/
+
+function handleDisconnect(client) {
+    client.on('error', function (error) {
+      if (!error.fatal) return;
+      if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+  
+      console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+  
+      // NOTE: This assignment is to a variable from an outer scope; this is extremely important
+      // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
+      // to `global.mysqlClient =` in node.
+      mysqlClient = mysql.createConnection(client.config);
+      handleDisconnect(mysqlClient);
+      mysqlClient.connect();
+    });
+  };
