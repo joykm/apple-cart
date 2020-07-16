aff2aaaf49621b6da5b47f7394b0ac8ca20d179e
George Kochera <54916850+gkochera@users.noreply.github.com>
2020-07-16 13:09:55 -0400

    Database Connection Fix (#6)
    * changed app.js to pool db connection instead of
    creating a single connection which disconnects
    and cause the DB to become unlinked
    
    * changed last updated date in header / app.js
    
    * cleared out commented code for clarity
    
    * fixed out-of-scope change to inventory route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 app.js
 
 Created On: 7/12/2020
-Last Updated On: 7/12/2020
+Last Updated On: 7/16/2020
 Description: Entry point for "The Apple Cart" web app, Store Inventory Management System
 */
 
@@ -51,22 +51,7 @@ if (process.env.NODE_ENV == 'development') {
 Database Setup and Configuration
 */
 
-const connection = mysql.createConnection(DATABASE_CREDENTIALS)
-handleDisconnect(connection)
-
-/*
-Commented out until we decide whether or not we want to use them
-Routing Constants
-  example use: 
-    const products = 'test-products';
-    "app.get('/' + products, function(...."
-    res.render(products, {data:data})
-*/
-// const home = 'home';
-// const inventory = 'inventory';
-// const product_list = "product_list"
-
-
+const connection = mysql.createPool(DATABASE_CREDENTIALS)
 
 /*
 Routing
@@ -91,14 +76,14 @@ app.get('/', function(req, res) {
     
 })
 
-
 // Product List Route
 app.get('/product_list', function(req, res) {
     res.render('product_list')
 })
 
+// Inventory Route
 app.get('/inventory', function(req, res) {
-    connection.query('SELECT * FROM `products` WHERE shelf_quantity <= shelf_min_threshold', function(error, results, fields){
+    connection.query('SELECT * FROM `products` WHERE shelf_quantity <= shelf_min_threshold`', function(error, results, fields){
         if (error) {
           console.log("Error in loading buyer page.")
         } 
@@ -115,19 +100,3 @@ app.listen(process.env.PORT)
 /*
 Error Handling
 */
-
-function handleDisconnect(client) {
-    client.on('error', function (error) {
-      if (!error.fatal) return;
-      if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
-  
-      console.error('> Re-connecting lost MySQL connection: ' + error.stack);
-  
-      // NOTE: This assignment is to a variable from an outer scope; this is extremely important
-      // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
-      // to `global.mysqlClient =` in node.
-      mysqlClient = mysql.createConnection(client.config);
-      handleDisconnect(mysqlClient);
-      mysqlClient.connect();
-    });
-  };
