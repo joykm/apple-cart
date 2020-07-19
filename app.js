59f56dd43f43726b92c628dba0618ee9b7cb7a01
blakelester <55535606+blakelester@users.noreply.github.com>
2020-07-19 11:51:06 -0600

    Cleaned up the App.js file. Made a few of the multiline SQL queries more readable. Added some additional error logging. (#16)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 app.js
 
 Created On: 7/12/2020
-Last Updated On: 7/16/2020
+Last Updated On: 7/19/2020
 Description: Entry point for "The Apple Cart" web app, Store Inventory Management System
 */
 
@@ -47,6 +47,7 @@ try {
 } catch(err) {
     var DATABASE_CREDENTIALS = process.env.CLEARDB_DATABASE_URL
 }
+
 /*
 Database Setup and Configuration
 */
@@ -62,9 +63,11 @@ app.get('/', function(req, res) {
 
     // Simple query to make sure the database is connected.
     var data = 'ClearDB Connected. Users are: '
-    connection.query('SELECT * FROM `users`', function(error, results, fields){
+
+    connection.query('SELECT * FROM users', function(error, results, fields){
         if (error) {
             data = 'ClearDB is down!'
+            console.log(error)
             res.render('home', {data: data})
         } else {
         results.forEach(element => {
@@ -76,14 +79,19 @@ app.get('/', function(req, res) {
     
 })
 
-/* Product Catalog Route
-   User will be able to view the content of proudct catalog
+/* 
+Product Catalog Route
+User will be able to view the content of proudct catalog
 */
 app.get('/product_catalog', function(req, res) {
-    /*Select Id, name, type, price, unit, description from products*/
-    connection.query('SELECT id, name, type, price, unit, description FROM products', function(error, results, fields){
+
+    // Change this to change the query going to the DB
+    var productCatalogQueryString = 'SELECT id, name, type, price, unit, description FROM products'
+
+    // Requesting the data from the database
+    connection.query(productCatalogQueryString, function(error, results, fields){
         if (error) {
-            console.log("Error loading product_catalog: " + error)
+            console.log('Error loading product_catalog: ' + error)
         }
         res.render('product_catalog', {results: results})
     })
@@ -93,17 +101,20 @@ app.get('/product_catalog', function(req, res) {
 app.get('/inventory', function(req, res) {
     
     // Change this to change the query going to the DB
-    var inventory_query_string = "SELECT id, name, shelf_quantity, DATE_FORMAT(exp_date,'%m-%d-%Y') AS exp_date, wh_quantity, " +
-    "shelf_quantity + wh_quantity AS total_quantity, " +
-    "shelf_min_threshold, shelf_max_threshold FROM products"
+    var inventoryQueryString =    
+        `SELECT id, name, shelf_quantity, DATE_FORMAT(exp_date,'%m-%d-%Y') AS exp_date, 
+        wh_quantity, shelf_quantity + wh_quantity AS total_quantity,
+        shelf_min_threshold, shelf_max_threshold FROM products;`
 
     // Requesting the data from the database
-    connection.query(inventory_query_string, function(error, results, fields){
+    connection.query(inventoryQueryString, function(error, results, fields){
         if (error) {
-          var data = "Error in querying the database."
+          var data = 'Error in querying the database.'
+          console.log(error)
           res.render('inventory', {data:data})
         }
 
+        console.log(results)
         // Check for items that are low on the shelf and set .shelf_low to true if they are
         results.forEach(function(value, index) {
             if (value.shelf_quantity < value.shelf_min_threshold) {
@@ -127,27 +138,29 @@ app.post('/inventory/new_item', function(req, res) {
     var shelf_inventory = req.body.product_shelf_inventory_input;
     var expiration_date = req.body.product_expiration_date_input;
 
-    // Form the SQL Query needed to updated the product inventory
-    var add_inventory_query_string = "UPDATE products SET " +
-    "exp_date = '" + expiration_date + "', " + 
-    "shelf_quantity = shelf_quantity + " + shelf_inventory + ", " +
-    "wh_quantity = wh_quantity + " + wh_inventory + " " + 
-    "WHERE name = '" + product_name + "'"
+    // Change this to change the query going to the DB
+    var addInventoryQueryString =
+        `UPDATE products 
+        SET exp_date = '${expiration_date}', shelf_quantity = shelf_quantity + ${shelf_inventory}, wh_quantity = wh_quantity + ${wh_inventory} 
+        WHERE name = '${product_name}'`
 
     // Send the query, if it fails, log to console, if it succeeds, update the screen.
-    connection.query(add_inventory_query_string, function(error, results, fields){
+    connection.query(addInventoryQueryString, function(error, results, fields){
         if (error) {
-            console.log("Add item inventory failed...")
+            console.log('Add item inventory failed...')
+            console.log(error)
         } else {
             res.redirect('/inventory')
         }
     })
 })
+
 /*
 Listener
 */
 
-app.listen(process.env.PORT)
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 /*
 Error Handling
