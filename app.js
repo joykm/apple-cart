5c6f8e01f60e0b08431f6b8066d9052c0cf9570e
George Kochera <kocherag@oregonstate.edu>
2020-07-28 17:01:21 -0400

    Removed debugging code from app.js

--- app.js
+++ app.js
@@ -257,7 +257,10 @@ app.post('/inventory/modify_item', function(req, res) {
     
     // Form the SQL Query needed to update the shelf thresholds
     var mod_inventory_query_string = "UPDATE products SET shelf_min_threshold = " +
-    min + ", shelf_max_threshold = " + max + "WHERE id = " + id
+    min + ", shelf_max_threshold = " + max + " WHERE id = " + id
+
+    // DEBUG
+    console.log(mod_inventory_query_string)
 
     // Send the query, if it fails, log to console, if it succeeds, update the row and refresh the data on the screen.
     connection.query(mod_inventory_query_string, function(error, results, fields) {
